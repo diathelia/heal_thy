4179e933a9d661b55fc8b7dda29ae2fc65036bb3
diathelia <37617288+diathelia@users.noreply.github.com>
2020-01-19 22:13:24 +1300

    polished

--- js/main.js
+++ js/main.js
@@ -1,4 +1,27 @@
-AOS.init({duration: '1000'}); // { disable: "mobile" }
+AOS.init({duration: '1000'}); // { disable: "mobile", easing: 'ease-in-out-back' }
+
+// init counter for aos:in:stripe AOS custom animation listener
+let c = 0;
+
+document.addEventListener('aos:in:stripe', ({detail}) => {
+  // increment counter
+  c++;
+
+  if ((c & 1) === 0) {
+    // is even --> reset positioning of element
+    detail.style.transform = 'translateY(0)';
+    // console.log(c, 'even, aos:out');
+  } else {
+    // is odd --> animate positioning of element
+    detail.style.transform = 'translateY(10vh)';
+    // console.log(c, 'odd, aos:in');
+  }
+});
+
+// not working ... gonna use even/odd triggers of aos:in instead
+// document.addEventListener('aos:out:super-duper', ({detail}) => {
+//   console.log('animated out', detail);
+// });
 
 // const buttons = document.querySelectorAll(".button"),
 //   weekArrow = document.querySelector("#week-arrow");
@@ -41,20 +64,34 @@ document.body.addEventListener("click", e => {
 // normalise where scroll-targets land between Firefox & Chrome with selector aliases
 const alias = {};
 // set default firefox scroll-targets
+alias.contact = '#stripe';
 alias.doctors = '.doc-grid';
+alias.top = '#contact';
 
-// if (window.chrome) {
-//   // overwrite aliases with chrome scroll-targets
-//   alias.ourTeam = ".chromePageTarget";
-//   alias.page-down = ".chromePageTarget";
-// }
+if (window.chrome) {
+  // overwrite aliases with chrome scroll-targets
+  alias.contact = '#stripe';
+  alias.doctors = '.doc-grid';
+  alias.top = '#contact';
+}
 
 // scroll listeners
+// contact button
+document.querySelector('#contact').addEventListener('click', e => {
+  e.preventDefault();
+  document.querySelector(alias.contact).scrollIntoView({behavior: 'smooth'});
+});
 
 // doctors button
 document.querySelector('#doctors').addEventListener('click', e => {
   e.preventDefault();
   document.querySelector(alias.doctors).scrollIntoView({behavior: 'smooth'});
+});
+
+// back to top button
+document.querySelector('#to-top').addEventListener('click', e => {
+  e.preventDefault();
+  document.querySelector(alias.top).scrollIntoView({behavior: 'smooth'});
 });
 
 // page-down arrow and goto: emergency hours
@@ -78,3 +115,43 @@ document.querySelector('#doctors').addEventListener('click', e => {
 // // drophours = document.querySelector("#drophours"),
 // // timetable = document.querySelector("#timetable"),
 // // btnTime = document.querySelectorAll("#drophours, #hours, #timetable");
+
+// ==========================================================================
+
+// init counter for aos:in:stripe AOS custom animation listener
+// let cStripe = 0;
+// let cInfo = 0;
+// let cUrgent = 0;
+
+// document.addEventListener('aos:in:stripe', ({detail}) => {
+//   // increment counter
+//   // c++;
+//   console.log(detail.id);
+//   if (detail.id === 'stripe') {
+//     cStripe++;
+//     ifEvenOdd(cStripe, detail, );
+//   } else if (detail.id === 'info') {
+//     cInfo++;
+//     ifEvenOdd(cInfo, detail, );
+//   } else if (detail.id === 'urgent') {
+//     cUrgent++;
+//     ifEvenOdd(cUrgent, detail, );
+//   } else {
+//     console.log('error, no detail.id');
+//   }
+// });
+
+// function ifEvenOdd(c, detail) {
+//   // bit test:
+//   // if c is odd, aos:in isn't lying
+//   // if c is even, aos:in is lying and should be aos:out
+//   if ((c & 1) === 0) {
+//     // is even --> reset positioning of element
+//     detail.style.transform = 'translateY(0)';
+//     // console.log(c, 'even, aos:out');
+//   } else {
+//     // is odd --> animate positioning of element
+//     detail.style.transform = 'translateY(20vh)';
+//     // console.log(c, 'odd, aos:in');
+//   }
+// }
