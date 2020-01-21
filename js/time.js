a559f4fe3f757ab4453a5a426b42321ce9c86a87
Diathelia <37617288+diathelia@users.noreply.github.com>
2020-01-22 01:23:29 +1300

    style and time.js tweaks

--- js/time.js
+++ js/time.js
@@ -22,7 +22,7 @@ const dateObj = new Date(),
   day = dateObj.getDay(),
   hour = dateObj.getHours(),
   min = dateObj.getMinutes(),
-  ids = ["#monday", "#tuesday", "#wednesday", "#thursday", "#friday"],
+  ids = ['#monday', '#tuesday', '#wednesday', '#thursday', '#friday'],
   today = [month + 1, date];
 
 let open = true,
@@ -55,20 +55,17 @@ holidays.otago = [3, 25];
 
 // get holidays by API
 async function fetchHolidays() {
-  const response = await fetch(
-    "https://holidayapi.com/v1/holidays?country=NZ&year=2018&key=34eab3f8-e49c-4c77-b9bd-17dfe3b24613",
-    {
-      method: "GET",
-      mode: "no-cors"
-    }
-  );
+  const response = await fetch('https://holidayapi.com/v1/holidays?country=NZ&year=2018&key=34eab3f8-e49c-4c77-b9bd-17dfe3b24613', {
+    method: 'GET',
+    mode: 'no-cors'
+  });
 
   // https://holidayapi.com/v1/holidays?pretty&country=NZ&year=2019&key=34eab3f8-e49c-4c77-b9bd-17dfe3b24613
   // https://publicholiday.co.nz/nz-public-holidays-2020.html
   // https://api.asb.co.nz/public/v1/public-holidays?countryCode=NZ&$fields=date,description
 
   const json = await response.text();
-  console.log("holidays:", JSON.stringify(json));
+  console.log('holidays:', JSON.stringify(json));
 }
 
 // modifies openDay date object, returns true/false
@@ -125,13 +122,13 @@ function isHoliday() {
 function threeDays() {
   // test proposed date object for holiday
   if (isHoliday() === true) {
-    console.log("openDay is a holiday, testing day after");
+    console.log('openDay is a holiday, testing day after');
     // set to 'closed'
     open = false;
     if (isHoliday() === true) {
-      console.log("still a holiday, testing day after");
+      console.log('still a holiday, testing day after');
       if (isHoliday() === true) {
-        console.log("still a holiday, last try then giving up");
+        console.log('still a holiday, last try then giving up');
         isHoliday();
       }
     }
@@ -141,26 +138,28 @@ function threeDays() {
 function applyHTML() {
   // if open is still true, then openDay is still today, therefore check todays hours/mins
   // if todays time is between 8:30am and 5:30pm set status to open
-  if (open === true && hour <= 17 && min <= 30 && hour >= 8 && min >= 30) {
-    document.querySelector("#status").style.color = "#060053"; // #56c9f8
-    statusText = "open";
-    openText = "open ⟶ ";
-  } else {
-    document.querySelector("#status").style.color = "red";
-    statusText = "closed";
-    openText = "next open ⟶ ";
+  if (open === true) {
+    if (hour <= 17 && min <= 30) {
+      if (hour >= 8 && min >= 30) {
+        document.querySelector('#status').style.color = '#060053'; // #56c9f8
+        statusText = 'open';
+        openText = 'open ⟶ ';
+      } else {
+        document.querySelector('#status').style.color = 'red';
+        statusText = 'closed';
+        openText = 'next open ⟶ ';
+      }
+    }
   }
 
   // add class to openText row
-  document.querySelector(ids[openDay.getDay() - 1]).classList.add("open-text");
+  document.querySelector(ids[openDay.getDay() - 1]).classList.add('open-text');
 
   // assign statusText to DOM
-  document.querySelector("#status").textContent = `${statusText}`;
+  document.querySelector('#status').textContent = `${statusText}`;
 
   // assign openText to DOM
-  document.querySelector(
-    ids[openDay.getDay() - 1]
-  ).firstElementChild.textContent = `${openText}`;
+  document.querySelector(ids[openDay.getDay() - 1]).firstElementChild.textContent = `${openText}`;
 }
 
 /*/ begin ordered execution *********************************/
@@ -173,7 +172,7 @@ threeDays();
 
 // test proposed non-holiday openDay
 if (isWeekend() === true) {
-  console.log("openDay pushed into weekend");
+  console.log('openDay pushed into weekend');
   // test monday tuesday wednesday for holidays, modifies openDay
   threeDays();
 }
